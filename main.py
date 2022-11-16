13b7ccca4244e5ca632547a7622b7cfb7a3a767e
Dmitri <72254723+Megaduox@users.noreply.github.com>
2022-11-16 11:50:12 +0300

    Final version

--- main.py
+++ main.py
@@ -1,13 +1,33 @@
 import requests
-import json
-import difflib
 
+from time import sleep
+
+import keys
 
 url = 'https://api-osmosis.imperator.co/pairs/v1/summary'
 
+all_tokens = set()
+all_tokens_temp = {'813', '835', '555', '810', '806',
+                   '704', '482', '600', '601', '572',
+                   '722', '719', '712', '611', '2',
+                   '681', '615', '796', '616', '679',
+                   '613', '605', '4', '690', '557', '778', '553',
+                   '560', '730', '567', '773', '627', '461',
+                   '662', '790', '197', '648', '8', '15', '772',
+                   '738', '1', '643', '817', '769', '608', '800',
+                   '837', '733', '585', '586', '629', '577', '626',
+                   '625', '812', '816', '674', '561', '9', '5', '602',
+                   '573', '777', '840', '7', '637', '731', '151', '562',
+                   '481', '612', '803', '793', '547', '617', '498', '649',
+                   '618', '644', '678', '22', '638', '571', '686', '744',
+                   '826', '787', '747', '651', '574', '597', '464', '604',
+                   '497', '606', '795', '596', '833', '584', '732', '641',
+                   '725', '3', '619', '832', '463', '645', '631', '42', '587',
+                   '558', '6', '13', '621'}
 
-def create_first_file():
-    all_tokens = list()
+
+def check_pools():
+
     response = requests.get(url)
 
     if response:
@@ -18,33 +38,24 @@ def create_first_file():
     json_response = response.json()
 
     for elem in json_response['data']:
-        all_tokens.append(elem['pool_id'])
+        all_tokens.add(elem['pool_id'])
 
-    file_object = open('firstFile.txt', 'w')
-    for item in all_tokens:
-        file_object.write("%s\n" % item)
-    file_object.close()
+    difference = all_tokens.difference(all_tokens_temp)
+    pair = list()
 
+    if difference:
+        all_tokens_temp.update(all_tokens)
 
-def compare_files():
-    result = list()
-    with open('firstFile.txt') as file_1:
-        file_1_text = file_1.readlines()
+        for elem in difference:
+            pair.append(get_pool_name(elem))
 
-    with open('secondFile.txt') as file_2:
-        file_2_text = file_2.readlines()
+        print(pair)
 
-    for line in difflib.unified_diff(
-            file_1_text, file_2_text, fromfile='firstFile.txt',
-            tofile='secondFile.txt', lineterm=''):
-        print(line)
-        result.append(line)
-    breakpoint()
+    return pair
 
 
-def check_new_tokens():
-    all_tokens = list()
-    response = requests.get(url)
+def get_pool_name(pool_id):
+    response = requests.get(f'https://api-osmosis.imperator.co/pools/v2/{pool_id}')
 
     if response:
         print('Ответ от сервера успешно получен!')
@@ -52,23 +63,32 @@ def check_new_tokens():
         print('В ходе осуществления запроса возникла ошибка.')
 
     json_response = response.json()
+    pair = f"{json_response[0]['symbol']}/{json_response[1]['symbol']}"
 
-    for elem in json_response['data']:
-        # print(i['quote_symbol'],'/',i['base_symbol'], sep = '')
-        # print('Цена:',round(i['price'],2))
-        # print('Ликвидность:',round(i['liquidity'],2),'\n')
-        # print('pool_address:', i['pool_address'],'\n')
-        # print(elem['pool_id'])
-        # if 'TORI' in elem['base_symbol']:
-        all_tokens.append(elem['pool_id'])
+    return pair
 
-    with open('secondFile.txt', 'w') as file_object:
-        for item in all_tokens:
-            file_object.write("%s\n" % item)
-        print('Done')
+
+def send_to_telegram(message):
+
+    apiToken = keys.apiToken
+    chatID = keys.chatID
+    apiURL = f'https://api.telegram.org/bot{apiToken}/sendMessage'
+
+    if message:
+        convert_message = '\n'.join(message)
+        try:
+            response = requests.post(apiURL, json={
+                'chat_id': chatID,
+                'text': f'В Osmosis добавлены следующие пулы:\n{convert_message}'
+            })
+            print(response.text)
+        except Exception as e:
+            print(e)
 
 
 if __name__ == '__main__':
-    # create_first_file()
-    # check_new_tokens()
-    compare_files()
+    while True:
+        pool = check_pools()
+        send_to_telegram(pool)
+        sleep(3600)
+
